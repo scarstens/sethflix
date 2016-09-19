e64d3d8691b5297fb20a9223a19d997b6f92b913
Seth Carstens <seth.carstens@gmail.com>
2016-09-18 21:17:41 -0700

    added recently added section to homepage

--- inc/plex_api_sdk_redux.class.php
+++ inc/plex_api_sdk_redux.class.php
@@ -1,53 +1,78 @@
 <?php
+
 /**
  * Class Plex_API_SDK_Redux
  * https://github.com/mjs7231/python-plexapi/wiki/Unofficial-Plex-API-Documentation
  */
 class Plex_API_SDK_Redux {
-	public static $plex_private = 'https://127.0.0.1';
+	public static $plex_private = 'http://server.sethflix.com:32400';
 	public static $plex_public = 'https://plex.tv/api';
 	private static $plex_server_token = PLEX_SERVER_TOKEN;
 
 	/**
 	 * @return array
 	 */
-	static function get_friends(){
+	static function get_friends() {
 
-		$request = \EasyRequest\Client::request(
-			self::$plex_public.'/users',
+		$request      = \EasyRequest\Client::request(
+			self::$plex_public . '/users',
 			'GET',
 			array(
 				'header' => array(
-					'User-Agent' => 'Firefox 45',
+					'User-Agent'   => 'Firefox 45',
 					'X-Plex-Token' => self::$plex_server_token
 				),
 			)
 		);
-		$response = $request->send();
+		$response     = $request->send();
 		$xml_response = $response->getBody()->getContents();
 
-		$data = self::plex_users_parser($xml_response);
+		$data = self::plex_users_parser( $xml_response );
 
 		return $data;
 	}
+
 
 	/**
 	 * @param $xml_string
 	 *
 	 * @return array
 	 */
-	static function plex_users_parser($xml_string) {
+	static function plex_users_parser( $xml_string ) {
 		$new_data = [];
-		$data = new SimpleXMLElement($xml_string);
+		$data     = new SimpleXMLElement( $xml_string );
 
-		foreach($data->children() as $user){
+		foreach ( $data->children() as $user ) {
 			$username = (string) $user->attributes()->username;
-			$username = substr($username, 0, strpos($username, "@"));
-			if( empty( $username ) ){
+			$username = substr( $username, 0, strpos( $username, "@" ) );
+			if ( empty( $username ) ) {
 				$username = (string) $user->attributes()->title;
 			}
-			foreach( $user->attributes() as $key => $user_data ){
-				$new_data[$username][$key] = (string) $user_data;
+			foreach ( $user->attributes() as $key => $user_data ) {
+				$new_data[ $username ][ $key ] = (string) $user_data;
+			}
+		}
+
+		return $new_data;
+	}
+
+	static function plex_sections_parser( $xml_string, $each = 'Video', $name_att = 'title' ) {
+		$new_data = [];
+		$data     = new SimpleXMLElement( $xml_string );
+		//echo '<pre style="overflow: scroll; height: 500px;">' . print_r( $data, true ) . '</pre>';
+		$i = 0;
+		foreach ( $data->children() as $child ) {
+			$i ++;
+			$label = (string) $child->attributes()->$name_att;
+			$label = substr( $label, 0, strpos( $label, "@" ) );
+			if ( empty( $label ) ) {
+				$label = (string) $child->attributes()->title;
+			}
+			if ( empty( $label ) ) {
+				$label = 'no_label_id__' . $i;
+			}
+			foreach ( $child->attributes() as $key => $data ) {
+				$new_data[ $label ][ $key ] = (string) $data;
 			}
 		}
 
@@ -58,7 +83,7 @@ class Plex_API_SDK_Redux {
 	 *
 	 * my.plexapp.com/users/sign_in.xml
 	 */
-	static function get_auth_code(){
+	static function get_auth_code() {
 
 	}
 
@@ -66,7 +91,42 @@ class Plex_API_SDK_Redux {
 	 *
 	 * plex.tv/api/resources
 	 */
-	static function get_servers(){
+	static function get_servers() {
 
+	}
+
+	static function get_recently_added_movies( $section = '1' ) {
+		$request      = \EasyRequest\Client::request(
+			self::$plex_private . '/library/sections/' . $section . '/recentlyAdded',
+			'GET',
+			array(
+				'header' => array(
+					'User-Agent'   => 'Plex_API_SDK_Redux(LinuxClient)',
+					'X-Plex-Token' => self::$plex_server_token
+				),
+			)
+		);
+		$response     = $request->send();
+		$xml_response = $response->getBody()->getContents();
+		$data         = self::plex_sections_parser( $xml_response );
+
+		return $data;
+	}
+
+	/**
+	 * @param string $section
+	 * TODO: fix security by forcing login and custom per login token
+	 */
+	public static function print_recently_added_movies( $section = '1' ) {
+		$movies = self::get_recently_added_movies( $section );
+		echo '<div class="recently-added"><style>.recently-added{padding-top:10px;}.recently-added:before { position: relative; top: -10px; left: 0px; font-size: 12px; font-weight: 700; color: #959595; text-transform: uppercase; letter-spacing: 1px; content: "Recently Added";}</style><div class="row">';
+		$i=0;
+		foreach ( $movies as $label => $movie ) {
+			$i++;
+			$thumb = self::$plex_private . $movie['thumb'] . '?X-Plex-Token='.self::$plex_server_token;
+			echo '<div class="col-xs-6 col-md-3"><a href="#" class="thumbnail"><img src="'.$thumb.'" alt="..."></a></div>';
+			if($i>3){break;}
+		}
+		echo '</div></div>';
 	}
 }
--- index.php
+++ index.php
@@ -1,45 +1,45 @@
 <?php
 //initialize application
-include_once('server_config.php');
+include_once( 'server_config.php' );
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php include_once('inc/part.head.php'); ?>
+	<?php include_once( 'inc/part.head.php' ); ?>
 </head>
 <body role="document">
-<?php include_once('inc/part.header-nav.php'); ?>
+<?php include_once( 'inc/part.header-nav.php' ); ?>
 <div class="container theme-showcase" role="main">
 	<div class="jumbotron">
 		<div class="container">
-		 <div class="row">
-        <div class="col-md-2">
-            <div class="imgAbt">
-                <img src="images/sethflix-icon.png" style="width: 100%; height: auto;" />
-            </div>
-        </div>
-        <div class="col-md-10">
-            <h1>Sethflix</h1>
-        </div>
-    </div>
+			<div class="row">
+				<div class="col-md-2">
+					<div class="imgAbt">
+						<img src="images/sethflix-icon.png" style="width: 100%; height: auto;"/>
+					</div>
+				</div>
+				<div class="col-md-10">
+					<h1>Sethflix</h1>
+				</div>
+			</div>
 		</div>
 		<p>Welcome to the new age of streaming media.</p>
 		<a href="http://plex.tv">
 			<button type="button" class="btn btn-default btn-upsize" aria-label="Left Align">
-			<span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span>
-			Plex Media Player
+				<span class="glyphicon glyphicon-facetime-video" aria-hidden="true"></span>
+				Plex Media Player
 			</button>
 		</a>
 		<a href="http://server.sethflix.com:3000/">
 			<button type="button" class="btn btn-default btn-upsize" aria-label="Left Align">
-			<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-			Plex Requests
+				<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+				Plex Requests
 			</button>
 		</a>
 		<a href="http://status.sethflix.com/">
 			<button type="button" class="btn btn-default btn-upsize" aria-label="Left Align">
-			<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
-			Sethflix Status
+				<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+				Sethflix Status
 			</button>
 		</a>
 		<a href="http://sethflix.com/friends.php">
@@ -48,6 +48,9 @@ include_once('server_config.php');
 				Sethflix Friends
 			</button>
 		</a>
+	</div>
+	<div class="recently-added-movies">
+		<?php Plex_API_SDK_Redux::print_recently_added_movies(); ?>
 	</div>
 </div>
 
